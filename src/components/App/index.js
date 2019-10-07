96321556f38a320ab8a5ed758d30985de98f74c2
Rahul Parkar <rahul.a.parkar@gmail.com>
2019-10-07 22:27:50 +0100

    Move app to components folder

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,10 @@
 
 import React from 'react'
+import styles from './index.scss'
 
 function App () {
   return (
-    <div className='App'>
+    <div className={styles.App}>
       Welcome to the App
     </div>
   )
